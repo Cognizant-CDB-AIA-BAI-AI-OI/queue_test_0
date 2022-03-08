76b88b2f37fef83573653ae87691e5edce7bc043
Aaryan369 <51906812+Aaryan369@users.noreply.github.com>
2022-03-08 16:08:50 +0530

    final test 0 --run

--- importer/import_test.py
+++ importer/import_test.py
@@ -3,3 +3,7 @@ import pandas as pd
 def my_func():
   df = pd.read_csv('cadec_csv.csv')
   print(df['Adverse Effects'][1])
+  
+  f = open('outputs/csv_out.txt','w+')
+  f.write(df['Adverse Effects'][0])
+  f.close()
