e2725daf0427c3871df12fc06f29f7c75a0db031
Aaryan369 <51906812+Aaryan369@users.noreply.github.com>
2022-04-13 08:33:25 +0530

    update 11 --run

--- hubert/basic.py
+++ hubert/basic.py
@@ -3,9 +3,9 @@ from transformers import HubertForCTC, Wav2Vec2Processor
 import torch
 import os
 import tensorflow as tf
-
+#"facebook/wav2vec2-base-960h"
 def s2t():
-  processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h"):
+  processor = Wav2Vec2Processor.from_pretrained()
   model = HubertForCTC.from_pretrained("facebook/hubert-large-ls960-ft") 
   
   print("number of gpus are: ",tf.test.is_gpu_available(cuda_only=True))
