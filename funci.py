510a7360c90b8332a5efe4ff8fb054f55d504461
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-01 17:30:17 +0530

    Update funci.py

--- funci.py
+++ funci.py
@@ -1,9 +1,9 @@
 import time
-
+import subprocess
 def adder():
   print('creating  a file')
   time.sleep(10)
-  
+  print(subprocess.check_output(['conda','env', 'list']).decode())
   f = open('outputs/a_Test_dile.txt','w+')
   f.write('hi hello world')
   f.close()
