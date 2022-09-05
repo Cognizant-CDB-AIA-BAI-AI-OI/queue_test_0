be01087af1e67db41500561f121b3014bda6852d
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-05 09:46:51 +0530

    demo test24 --run

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from .funci import adder
 import subprocess
 def config():
   #subprocess.run('conda init bash', shell = True)
-  subprocess.run('source ./opt/conda/etc/profile.d/conda.sh')
+  subprocess.run('source /opt/conda/etc/profile.d/conda.sh',shell = True)
   subprocess.run('conda activate stats', shell=True)
   adder() 
   
