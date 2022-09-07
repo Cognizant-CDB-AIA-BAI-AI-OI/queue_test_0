c2a8bc0ab8531558f4b65cdcd57a1529458713e4
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-07 17:41:22 +0530

    Update funci.py

--- funci.py
+++ funci.py
@@ -2,6 +2,8 @@ import time
 import subprocess
 def adder():
   print('creating  a file')
+  print(subprocess.run('conda activate stats', shell=True))
+  print('comma')
   time.sleep(10)
   print(subprocess.check_output(['conda','env', 'list']).decode())
   print(subprocess.check_output("conda env list | grep '*'", shell=True, encoding='utf-8'))
