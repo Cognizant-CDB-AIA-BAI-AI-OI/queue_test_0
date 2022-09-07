83f3cb51c953e1dcd33437059bffdba5f7114b51
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-07 18:41:41 +0530

    demo test38 --run

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ def config():
   #subprocess.run('conda init bash', shell = True)
   #subprocess.run('conda ~/root/.bashrc',shell = True)
   #subprocess.run('conda install --stats', shell=True)
-  subprocess.run('conda env create -f environment.yml', shell=True)
-  subprocess.run('conda activate stats', shell=True)
+  #subprocess.run('conda env create -f environment.yml', shell=True)
+  #subprocess.run('conda activate stats', shell=True)
   adder() 
   
   
