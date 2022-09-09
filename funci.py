83a5b7f9a70f0bcfbaf1dd0c1270cd6d346b1ab3
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-09 11:56:54 +0530

    Update funci.py

--- funci.py
+++ funci.py
@@ -5,7 +5,7 @@ def adder():
   time.sleep(10)
   print(subprocess.check_output(['conda','env', 'list']).decode())
   print(subprocess.check_output("conda env list | grep '*'", shell=True, encoding='utf-8'))
-  f = open('outputs/a_Test_dile.txt','w+')
+  f = open('outputs/a_test_dile.txt','w+')
   f.write(subprocess.check_output("conda env list | grep '*'", shell=True, encoding='utf-8'))
   #f.write('hi hello world')
   f.close()
