5b1dfb9c1fb67d1b756fed4e83a9dcc4a9d8fd29
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-12 09:47:49 +0530

    demo test52 --run

--- config.py
+++ config.py
@@ -7,13 +7,7 @@ def config():
   #subprocess.run('conda env create -f environment.yml', shell=True)
   #subprocess.run('conda activate stats', shell=True)
   adder() 
-  
-  
-  
-  
-  
-  
-  
+ 
   
   
   
