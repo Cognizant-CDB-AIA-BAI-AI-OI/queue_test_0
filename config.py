8d3f5c291ba93615e5a6516b2395080b12f5222d
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-23 11:42:26 +0530

    demo test70 --run

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 #from .hubert import basic
-from .funci import adder
+#from .funci import adder
 import subprocess
 def config():
   #subprocess.run('conda init bash', shell = True)
-  
+  subprocess.run('conda run -n stats1 python funci.py',shell =True)
   #subprocess.run('conda env create -f environment.yml', shell=True)
   #subprocess.run('conda activate stats', shell=True)
   #subprocess.run('cd home/jupyter/Mukesh4/Queue',shell= True)
   #subprocess.run('rq worker --with-scheduler',shell =True)
-  adder() 
+  #adder() 
 
   
   
