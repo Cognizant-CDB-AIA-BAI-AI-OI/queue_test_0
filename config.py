4ab01ef487768a96d983016f7cb2e2a879d1cf9b
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-26 10:11:33 +0530

    demo test78 --run

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import subprocess
 def config():
   #subprocess.run('conda init bash', shell = True)
   print('config running')
-  subprocess.run('conda run -n stats1 python queue_test_0/funci.py',shell =True)
+  subprocess.run('conda run -n env4 python queue_test_0/funci.py',shell =True)
   print('done')
   #subprocess.run('conda env create -f environment.yml', shell=True)
   #subprocess.run('conda activate stats', shell=True)
