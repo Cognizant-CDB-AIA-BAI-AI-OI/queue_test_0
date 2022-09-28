31947ef12ad35131a11eed10b138a958d014f9d3
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-28 09:56:38 +0530

    demo test87 --run

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-#from .hubert import basic
-#from .funci import adder
+from .hubert import basic
+from .funci import adder
 import subprocess
 import time
 def config():
@@ -8,15 +8,15 @@ def config():
   #subprocess.run('conda env create -f queue_test_0/env11.yml',shell =True)
   #time.sleep(100)
   #subprocess.run('conda run -n env11 python queue_test_0/funci.py',shell =True)
-  subprocess.run('conda run -n gradio python queue_test_0/funci.py',shell =True)
-  print('done')
+  #subprocess.run('conda run -n gradio python queue_test_0/funci.py',shell =True)
+  #print('done')
   
   
   #subprocess.run('conda env create -f environment.yml', shell=True)
   #subprocess.run('conda activate stats', shell=True)
   #subprocess.run('cd home/jupyter/Mukesh4/Queue',shell= True)
   #subprocess.run('rq worker --with-scheduler',shell =True)
-  #adder() 
+  adder() 
 
   
   
