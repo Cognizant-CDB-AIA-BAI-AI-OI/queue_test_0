d4f3f7468cce951f43b9dd3ddae3108ef938db70
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-09-28 09:58:37 +0530

    demo test88 --run

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ def config():
   #subprocess.run('conda run -n gradio python queue_test_0/funci.py',shell =True)
   #print('done')
   
-  
+ 
   #subprocess.run('conda env create -f environment.yml', shell=True)
   #subprocess.run('conda activate stats', shell=True)
   #subprocess.run('cd home/jupyter/Mukesh4/Queue',shell= True)
