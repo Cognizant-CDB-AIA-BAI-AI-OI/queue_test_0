5e275fdd47653fc0c3c7d982eefd1737c2860aab
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-10-11 10:21:17 +0530

    Update funci.py

--- funci.py
+++ funci.py
@@ -22,7 +22,7 @@ def adder() :
     with gzip.GzipFile(fileobj=io.BytesIO(content), mode='rb') as f:
       content = f.read()
     return content.decode(encoding)
-  content1=read_gzip_txt_file('file.txt.gz')
+  content1=read_gzip_txt_file('queue_test_0/file.txt.gz')
   f = open('outputs/a_test_dile.txt','w+')
   f.write(len(content1))
   f.close()
