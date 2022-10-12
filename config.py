69d5872143635d6edc65257d47bd8e324a571679
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-10-12 09:44:40 +0530

    demo test114 --run

--- config.py
+++ config.py
@@ -6,6 +6,11 @@ def config():
   
   print('config running')
   subprocess.run('conda env create -f queue_test_0/env3.yml',shell =True)
+  
+  f = open('outputs/a_test_dile.txt','w+')
+  f.write('Environment Created')
+  f.close()
+  
   time.sleep(150)
   subprocess.run('conda run -n env3 python queue_test_0/funci.py',shell =True)
   
