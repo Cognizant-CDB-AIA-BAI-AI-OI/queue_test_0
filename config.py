09782d6711b3a21671b7d4d3f4805c63a96628fc
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-10-12 10:15:21 +0530

    demo test118 --run

--- config.py
+++ config.py
@@ -2,17 +2,30 @@
 #from .funci import adder
 import subprocess
 import time
+
 def config():
   
   print('config running')
-  subprocess.run('conda env create -f queue_test_0/env3.yml',shell =True)
   
-  f = open('outputs/a_test_dile.txt','w+')
-  f.write('Environment Created')
-  f.close()
+  try :
+    import sys 
+    subprocess.run('conda env create -f queue_test_0/env3.yml',shell =True)
   
-  time.sleep(150)
-  subprocess.run('conda run -n env3 python queue_test_0/funci.py',shell =True)
+  except :
+    
+    
+    f = open('outputs/a_test_dile.txt','w+')
+    f.write(str(sys.exc_info()[0]))
+    f.close()
+  
+  else :
+  
+    f = open('outputs/a_test_dile.txt','w+')
+    f.write('Environment Created')
+    f.close()
+  
+    time.sleep(150)
+    subprocess.run('conda run -n env3 python queue_test_0/funci.py',shell =True)
   
   
   
