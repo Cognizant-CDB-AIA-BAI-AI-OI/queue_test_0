9fcb9b4b53fee627b240c36ecc1c530fb15848d6
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-11-03 13:39:27 +0530

    Update funci.py

--- funci.py
+++ funci.py
@@ -14,10 +14,6 @@ import pandas as pd
 @pysnooper.snoop('outputs/file.log')
 
 def adder() :
-  #print('creating  a file')
-  #time.sleep(10)
-  #print(subprocess.check_output(['conda','env', 'list']).decode())
-  #print(subprocess.check_output("conda env list | grep '*'", shell=True, encoding='utf-8'))
   
   
   def read_gzip_txt_file(file_path: str, encoding: str = 'utf-8') -> str:
@@ -34,27 +30,7 @@ def adder() :
   f.write(str(len(content1)))
   f.close()
   
-"""  
-  try :
-    content1=read_gzip_txt_file('queue_test_0/file.txt')
-    f = open('outputs/a_test_dile.txt','a+')
-    f.write(str(len(content1)))
-    f.close()
-    
-  except :
-    f = open('outputs/a_test_dile.txt','a+')
-    f.write(str(sys.exc_info()[0]))
-    f.close()
-    
-  
-  
-  #f = open('outputs/a_test_dile.txt','w+')
-  #f.write(subprocess.check_output("conda env list | grep '*'", shell=True, encoding='utf-8'))
-  #f.write('hi hello world')
-  #f.close()
-"""  
- 
-  #f.close()
+
 adder()
   
   
